2109a38711e570900cca16308fc380fa7ed17a47
lcurtis4 <lcurtis0104@gmail.com>
2021-02-01 14:43:55 -0600

    login form to test

---
+++ src/Meal.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom"
+import { ApplicationViews } from "./ApplicationViews";
+import { Login } from "./components/auth/Login";
+import { Register } from "./components/auth/Register"
+import "./Meal.css";
+
+export const Meal = () => ( 
+    <>
+    <Route
+        render={() => {
+        if (localStorage.getItem("meal_user")) {
+            return (
+            <>
+                <ApplicationViews />
+            </>
+            );
+        } else {
+            return <Redirect to="/login" />;
+        }
+        }}
+    />
+
+    <Route path="/login">
+        <Login />
+    </Route>
+    <Route path="/register">
+        <Register />
+    </Route>
+    </>
+);
---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,18 @@
+/* 
+import React from "react"
+import { Link } from "react-router-dom"
+import "./NavBar.css"
+
+export const NavBar = (props) => {
+    return ( 
+        <ul className="navbar">
+            <li className="navbar_item active">
+                <Link className="navbar__link" to="/meals"> My Meals </Link>
+            </li>
+            <li className="navbar_item">
+                <Link className="navbar__link" to="/specials"> Today's Specials </Link>
+            </li>
+        </ul>
+    )
+}
+*/
