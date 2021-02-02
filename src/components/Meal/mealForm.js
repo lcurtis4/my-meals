a14ccfdbc4f618f4ff1d28307142a1b32f029fb8
lcurtis4 <lcurtis0104@gmail.com>
2021-02-02 16:03:29 -0600

    fixing merge stuff

---
+++ src/components/Meal/mealForm.js
@@ -0,0 +1,19 @@
+/* TODO: 
+    Create form elements for dish name
+    Create form elements for main dish
+    Create form elements for side dish
+    Create form elements for beverage
+    Create form elements for side dish
+
+    create dropdown to select meal type
+        options:
+            breakfast 
+            lunch 
+            dinner
+
+    create save button
+
+    make them all save to the database 
+
+    boom you're done kid!
+     */
