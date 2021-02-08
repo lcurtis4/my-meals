88dae58d0016246ed3b4bd99d47071c281e8ce13
lcurtis4 <lcurtis0104@gmail.com>
2021-02-08 15:45:11 -0600

    logic for specials dropdown

--- src/components/Specials/SpecialDropDown.js
+++ src/components/Specials/SpecialDropDown.js
@@ -1,21 +1,44 @@
-import { useState } from "react";
+import { useState, useContext, useEffect } from "react";
+import { MealContext } from "../Meal/MealProvider";
 
 export const SpecialDrpdwn = () => {
-    const [meal, setMeal] = useState({
-        name: "",
-        mealTypeId: 0,
-        mainDish: "",
-        sideDish: "",
-        bev: "",
-    });
+    const [breakfastChoices, setBreakfastChoices] = useState([]);
+    const [lunchChoices, setLunchChoices] = useState([]);
+    const [dinnerChoices, setDinnerChoices] = useState([]);
+
+    const { meals, getMeals } = useContext(MealContext);
+
+    useEffect(() => {
+        getMeals()
+    }, []);
+    
+    useEffect(() => {
+        /* loop over meals, add the meal to the corresponding array. */ 
+        const breakfast = []
+        const lunch = []
+        const dinner = []
+        for (const meal of meals ) {
+            if (meal.mealTypeId === 1 ) {
+                breakfast.push(meal)
+            } else if (meal.mealTypeId ===  2 ){
+                lunch.push(meal)
+            } else {
+                dinner.push(meal)
+            }
+        }
+        setBreakfastChoices(breakfast) 
+        setLunchChoices(lunch)
+        setDinnerChoices(dinner)
+
+    },[meals])
 
     const handleControlledInputChange = (event) => {
-        const newMeal = { ...meal };
+        // const newMeal = { ...meal };
 
-        newMeal[event.target.id] = event.target.value;
+        // newMeal[event.target.id] = event.target.value;
 
-        setMeal(newMeal);
-        console.log(meal);
+        // setMeal(newMeal);
+        // console.log(meal);
     };
 
 
@@ -33,6 +56,11 @@ export const SpecialDrpdwn = () => {
             onChange={handleControlledInputChange}
             >
             <option value="">Please Select a Breakfast</option>
+            {breakfastChoices.map((meal) => {
+                return <option value="{meal.id}"> 
+                    {meal.name}
+                </option> 
+            })}
         </select>
         </div>
     </fieldset>
@@ -49,6 +77,11 @@ export const SpecialDrpdwn = () => {
             onChange={handleControlledInputChange}
             >
             <option value="">Please Select a Lunch</option>
+            {lunchChoices.map((meal) => {
+                return <option value="{meal.id}"> 
+                    {meal.name}
+                </option> 
+            })}
         </select>
         </div>
     </fieldset>
@@ -65,6 +98,11 @@ export const SpecialDrpdwn = () => {
             onChange={handleControlledInputChange}
             >
             <option value="">Please Select a Dinner</option>
+            {dinnerChoices.map((meal) => {
+                return <option value="{meal.id}"> 
+                    {meal.name}
+                </option> 
+                })}
         </select>
         </div>
     </fieldset>
